e6c4e32dbcc233d931336afcd5bac804b5c05f61
qanusha436 <qanusha436@gmail.com>
2025-04-15 14:43:54 +0530

    add file to main

---
+++ cie1.java
@@ -0,0 +1,18 @@
+public class Cie1 {
+    public static void main(String[] args) {
+    
+        int a = 12, b = 5;
+    
+        // addition operator
+        System.out.println("a + b = " + (a + b));
+    
+        // subtraction operator
+        System.out.println("a - b = " + (a - b));
+    
+        // multiplication operator
+        System.out.println("a * b = " + (a * b));
+    
+        // division operator
+        System.out.println("a / b = " + (a / b));
+      }
+    }
